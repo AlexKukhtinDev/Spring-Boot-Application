22f656a62bea151119ddeb3671ca4f28ee4e1cac
akukhtin <alex.kukhtin.dev@gmail.com>
2020-01-16 18:43:53 +0200

    fixed mistakes and add lombok property in Pom.xml

--- src/main/java/com/akukhtin/springbootapplication/model/UserFromCanada.java
+++ src/main/java/com/akukhtin/springbootapplication/model/UserFromCanada.java
@@ -11,4 +11,20 @@ public class UserFromCanada extends User {
         this.provinceName = provinceName;
         this.currentCity = currentCity;
     }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getCurrentCity() {
+        return currentCity;
+    }
+
+    public void setCurrentCity(String currentCity) {
+        this.currentCity = currentCity;
+    }
 }
