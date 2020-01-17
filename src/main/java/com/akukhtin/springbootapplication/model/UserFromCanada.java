8079136650e264d0cfc7febcbe0727f60ede1250
akukhtin <alex.kukhtin.dev@gmail.com>
2020-01-17 14:19:54 +0200

    delete constructor to UserFromCanada

--- src/main/java/com/akukhtin/springbootapplication/model/UserFromCanada.java
+++ src/main/java/com/akukhtin/springbootapplication/model/UserFromCanada.java
@@ -4,14 +4,6 @@ public class UserFromCanada extends User {
     private String provinceName;
     private String currentCity;
 
-    public UserFromCanada(String name, String surname,
-                          char[] password, Country country,
-                          String provinceName,String currentCity) {
-        super(name, surname, password, country);
-        this.provinceName = provinceName;
-        this.currentCity = currentCity;
-    }
-
     public String getProvinceName() {
         return provinceName;
     }
