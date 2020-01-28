73de115f2157491a9577f0f5387b90881a33e774
akukhtin <alex.kukhtin.dev@gmail.com>
2020-01-28 10:10:14 +0200

    add travis and fix mistakes

--- src/main/java/com/akukhtin/springbootapplication/Application.java
+++ src/main/java/com/akukhtin/springbootapplication/Application.java
@@ -3,8 +3,10 @@ package com.akukhtin.springbootapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
 @EnableJpaRepositories("com.akukhtin.springbootapplication.repository")
 @EntityScan("com.akukhtin.springbootapplication.model")
 @SpringBootApplication
--- src/main/java/com/akukhtin/springbootapplication/repository/UserRepository.java
+++ src/main/java/com/akukhtin/springbootapplication/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-    List<User>findAllById(Long id);
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllById(Long id);
 }
