8f6200e72d03c93d3a1040bfbc83002bd6930617
Alexander <46680341+AlexKukhtinDev@users.noreply.github.com>
2020-01-31 10:40:29 +0200

    add travis and fix mistakes (#3)
    * add travis and fix mistakes
    
    * add travis and fix mistakes, add empty line
    
    * check travis
    
    * fixed travis
    
    * delete test default

---
+++ src/main/java/com/akukhtin/springbootapplication/model/UserFromUsa.java
@@ -0,0 +1,10 @@
+package com.akukhtin.springbootapplication.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserFromUsa extends User {
+
+    private List<UserFromUsa> listOfNeighboringStates = new ArrayList<>();
+
+}
