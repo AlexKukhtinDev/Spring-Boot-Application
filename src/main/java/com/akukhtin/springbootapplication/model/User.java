d02f8666519d4f5a7e212d2c0196735a9ad81e6c
akukhtin <alex.kukhtin.dev@gmail.com>
2020-02-04 13:19:05 +0200

    Added role

--- src/main/java/com/akukhtin/springbootapplication/model/User.java
+++ src/main/java/com/akukhtin/springbootapplication/model/User.java
@@ -1,10 +1,17 @@
 package com.akukhtin.springbootapplication.model;
 
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +25,7 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
     private Long id;
     @Column(name = "name")
     private String name;
@@ -30,6 +38,12 @@ public class User {
     private Country country;
     @Column(name = "email")
     private String email;
+    @ManyToMany(cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "role_id"))
+    private Set<Role> roles =new HashSet<Role>();
 
     public User(String name, String surname, String password, Country country, String email) {
         this.name = name;
@@ -37,5 +51,24 @@ public class User {
         this.password = password;
         this.country = country;
         this.email = email;
+    }
+
+    public User(String name, String surname, String password, String confirmPassword,
+                Country country, String email, Set<Role> roles) {
+        this.name = name;
+        this.surname = surname;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.country = country;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public Set<String> getRolesString() {
+        Set<String> list = new HashSet<>();
+        for (Role role : roles) {
+            list.add(role.getRoleName());
+        }
+        return list;
     }
 }
